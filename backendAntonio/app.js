3709b36238439588c95701b7164b4aac6ff572d6
Antonio Cuellar <lozano.roberto.2018@gmail.com>
2022-05-05 22:01:02 -0500

    Create the backend boilerplate, necessary modules, create mongodb schema to user login and sessions.

---
+++ backendAntonio/app.js
@@ -0,0 +1,62 @@
+//jshint esversion:6
+/// Necessary modules
+require('dotenv').config(); //// For environment variables 
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require("lodash");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const app = express();
+
+app.set('view engine', 'ejs');/// Set the engine to render the ejs
+app.use(bodyParser.urlencoded({extended: true}));/// Url encoded to retrive html names and values
+app.use(express.static("public")); /// Server dir
+
+/// Mongoose connection configuration
+const url = "mongodb://localhost:27017/userDB"; /// LocalHost Route to userDB
+mongoose.connect(url, {useNewUrlParser: true});
+
+/// Conection container 
+const wordone = process.env.WORDONE;
+
+/// Use modules
+/// Users sesions configuration
+app.use(session({
+    secret: wordone,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize()); /// init passport 
+app.use(passport.session()); /// passport handle sessions
+
+/// Initial schema for the databe
+
+const userSchema = new mongoose.Schema({
+    userName: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose); // seting the userchema as a mongoose local plugin
+
+const User = new mongoose.model("User",userSchema); /// Mongoose model
+/// Passport module configurations
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+/// REQUESTS
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+
+const port = process.env.PORT;
+
+app.listen(port, function() {
+    console.log("Server now is Listen");
+});
